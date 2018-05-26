87a9cf838b4a903a440891edb40f80f13deb42d0
Zoe Ames <zoeames@gmail.com>
2018-05-26 09:47:49 -0500

    images displaying

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,9 +1,9 @@
-const domString = (movieArray) => {
+const domString = (movieArray, config) => {
   let strang = '';
   movieArray.forEach((movie) => {
     strang += `<div class="col-sm-6 col-md-4">`;
     strang +=   `<div class="thumbnail">`;
-    strang +=     `<img src="..." alt="...">`;
+    strang +=     `<img src="${config.base_url}/w342/${movie.poster_path}" alt="Movie Poster">`;
     strang +=     `<div class="caption">`;
     strang +=       `<h3>${movie.original_title}</h3>`;
     strang +=       `<p>${movie.overview}</p>`;
