8bd08c92daa6e762e9b18ebda83e8007881db8e4
Callan Morrison <callan.l.morrison@gmail.com>
2018-06-02 11:47:05 -0500

    adding filter functionality

--- javascripts/firebaseApi.js
+++ javascripts/firebaseApi.js
@@ -42,6 +42,50 @@ const getAllMovies = () => {
   });
 };
 
+const getWatchedMovies = () => {
+  return new Promise((resolve, reject) => {
+    const allMoviesArray = [];
+    $.ajax({
+      method: 'GET',
+      url: `${firebaseConfig.databaseURL}/movies.json?orderBy="isWatched"&equalTo=true`,
+    })
+      .done((allMoviesObj) => {
+        if (allMoviesObj !== null) {
+          Object.keys(allMoviesObj).forEach((fbKey) => {
+            allMoviesObj[fbKey].id = fbKey;
+            allMoviesArray.push(allMoviesObj[fbKey]);
+          });
+        }
+        resolve(allMoviesArray);
+      })
+      .fail((error) => {
+        reject(error);
+      });
+  });
+};
+
+const getWishlistMovies = () => {
+  return new Promise((resolve, reject) => {
+    const allMoviesArray = [];
+    $.ajax({
+      method: 'GET',
+      url: `${firebaseConfig.databaseURL}/movies.json?orderBy="isWatched"&equalTo=false`,
+    })
+      .done((allMoviesObj) => {
+        if (allMoviesObj !== null) {
+          Object.keys(allMoviesObj).forEach((fbKey) => {
+            allMoviesObj[fbKey].id = fbKey;
+            allMoviesArray.push(allMoviesObj[fbKey]);
+          });
+        }
+        resolve(allMoviesArray);
+      })
+      .fail((error) => {
+        reject(error);
+      });
+  });
+};
+
 const deleteMovieFromDb = (movieId) => {
   return new Promise((resolve, reject) => {
     $.ajax({
@@ -77,6 +121,8 @@ module.exports = {
   setConfig,
   saveMovieToWishlist,
   getAllMovies,
+  getWatchedMovies,
+  getWishlistMovies,
   deleteMovieFromDb,
   updateMovieToWatchedInDb,
 };
