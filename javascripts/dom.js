b7f63721bde8b429500e9da3d0303840a1241fc6
Callan Morrison <callan.l.morrison@gmail.com>
2018-06-02 09:38:59 -0500

    getting movies and printing to dom correctly

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,5 +1,5 @@
 
-const domString = (movieArray, config, whereToPrint) => {
+const domString = (movieArray, config, whereToPrint, myCollectionMode = false) => {
   let strang = '';
   movieArray.forEach((movie, index) => {
     if (index % 3 === 0) {
@@ -7,12 +7,20 @@ const domString = (movieArray, config, whereToPrint) => {
     }
     strang += `<div class="col-sm-6 col-md-4">`;
     strang +=   `<div class="thumbnail movie">`;
+    if (myCollectionMode) {
+      strang += `<a class="btn deleteMovieFromCollectionEvent">X</a>`;
+    }
     strang +=     `<img data-poster="${movie.poster_path}" src="${config.base_url}/w342/${movie.poster_path}" alt="Movie Poster">`;
     strang +=     `<div class="caption">`;
     strang +=       `<h3 class="movie-title">${movie.original_title ? movie.original_title : movie.title}</h3>`;
     strang +=       `<p class="movie-overview">${movie.overview}</p>`;
-    strang +=       `<p><a href="#" class="btn btn-primary" role="button">Review</a> 
-    <a class="btn btn-default addMovieToWishlist" role="button">Wishlist</a></p>`;
+    if (!myCollectionMode) {
+      strang +=     `<p><a class="btn btn-default addMovieToWishlist" role="button">Wishlist</a></p>`;
+    } else if (myCollectionMode && !movie.isWatched) {
+      strang += `<p><a class="btn btn-primary updateMovieToWatched" role="button">I've Watched It</a></p>`;
+    } else {
+      strang += `<p>I'm going to put star rating here one day.</p>`;
+    }
     strang +=     `</div>`;
     strang +=   `</div>`;
     strang += `</div>`;
