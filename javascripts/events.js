4107b79c9340d880761b75ca38d569d31779fca6
rousell <lauren.l.rouse@gmail.com>
2018-06-04 19:40:31 -0500

    Added Login and Registration forms, plus basic login functionality

--- javascripts/events.js
+++ javascripts/events.js
@@ -27,7 +27,7 @@ const myLinks = () => {
 const pressEnter = () => {
   // big old keypress event
   $(document).keypress((e) => {
-    if (e.key === 'Enter') {
+    if (e.key === 'Enter' && !$('#search').hasClass('hide')) {
       const searchWords = $('#searchBar').val().replace(' ', '%20');
       tmdb.showResults(searchWords);
     }
@@ -133,6 +133,36 @@ const filterEvents = () => {
   });
 };
 
+const authEvents = () => {
+  $('#signin-btn').click((e) => {
+    e.preventDefault();
+    const email = $('#inputEmail').val();
+    const pass = $('#inputPassword').val();
+    firebase.auth().signInWithEmailAndPassword(email, pass)
+      .then((user) => {
+        $('#myMovies').removeClass('hide');
+        $('#search').addClass('hide');
+        $('#authScreen').addClass('hide');
+        // call the getMoviesEvent
+        getAllMoviesEvent();
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        console.error(errorMessage);
+      });
+  });
+
+  $('#register-link').click(() => {
+    $('#login-form').addClass('hide');
+    $('#registration-form').removeClass('hide');
+  });
+
+  $('#signin-link').click(() => {
+    $('#login-form').removeClass('hide');
+    $('#registration-form').addClass('hide');
+  });
+};
+
 const initializer = () => {
   myLinks();
   pressEnter();
@@ -140,6 +170,7 @@ const initializer = () => {
   deleteMovieFromFirebase();
   updateMovieEent();
   filterEvents();
+  authEvents();
 };
 
 module.exports = {
